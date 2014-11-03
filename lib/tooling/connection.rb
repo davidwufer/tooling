51fca8ac8d7770c825deddc26f9bb1c22b656584
David Wu <davidcnwu@gmail.com>
2014-11-03 08:47:10 -0800

    Adding 'get' command

--- lib/tooling/connection.rb
+++ lib/tooling/connection.rb
@@ -5,12 +5,15 @@ module Tooling
   class Connection
     attr_reader :faraday
 
-    BASE_URL = "https://na17.salesforce.com/services/data/v32.0/tooling"
-
     def initialize(args = {})
       @faraday = args[:faraday] || Faraday.new
       @access_token = args[:access_token]
       @base_tooling_url = create_base_tooling_url(args)
+    end
+
+    def get(sobject, id)
+      extension_url = ['sobjects', sobject, id].join('/')
+      request(extension_url)
     end
 
     def describe(sobject)
@@ -26,7 +29,6 @@ module Tooling
     def request(extension_url)
       full_url = [@base_tooling_url, extension_url].join('/')
 
-      puts full_url
       faraday.get do |req|
         req.url full_url
         req.headers['Authorization'] = "Bearer #{@access_token}"
@@ -37,7 +39,10 @@ module Tooling
       def create_base_tooling_url(args)
         [
           args[:instance_url] || "https://na1.salesforce.com",
-          'services', 'data', "v#{args[:api_version]}", 'tooling'
+          'services',
+          'data',
+          "v#{args[:api_version]}",
+          'tooling'
         ].join('/')
       end
   end
